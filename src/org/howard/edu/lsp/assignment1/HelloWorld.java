7f4641c33fac189a7ded57e84c95d41da4787105
Incheon Robotics <incheonrobotics@gmail.com>
2026-08-18 13:46:03 -0400

    Set up README.md and Added Hello World file

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* This simple print statementruns "Hello World!!" to the console.
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+        public static void main(String[] args) {
+        System.out.println("\"Hello World!!\"");
+    }
+}
