b947e4910986016c0b4ba3f60bcca8a2261da04f
Tstann19 <trinity.stann@bison.howard.edu>
2026-08-18 13:56:59 -0400

    Created README.md and Hello World file

---
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This simple print statement runs "Hello World!!" to the console.
+ * @author Trinity St Ann
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
